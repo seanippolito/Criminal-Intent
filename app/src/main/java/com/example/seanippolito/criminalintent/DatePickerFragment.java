0e79fabf5271a97a45eb4b234cdd9c7d4c4c7ad8
Sean Ippolito <ippolitosean@gmail.com>
2017-03-08 13:48:59 -0800

    added file for date fragment

---
+++ app/src/main/java/com/example/seanippolito/criminalintent/DatePickerFragment.java
@@ -0,0 +1,12 @@
+package com.example.seanippolito.criminalintent;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by seanippolito on 3/8/17.
+ * This is the Question object to be used across QuizActivity
+ */
+
+public class DatePickerFragment extends DialogFragment {
+    
+}
